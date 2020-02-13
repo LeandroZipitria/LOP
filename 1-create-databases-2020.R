f98cf13ba7ff4ab1e021a72f9464b9e24b715d5e
Leandro Zipitria <leandro.zipitria@gmail.com>
2020-02-12 23:49:55 -0300

    2020 new R files

--- 1-create-databases-2020.R
+++ 1-create-databases-2020.R
@@ -16,6 +16,7 @@ library(readxl)
 
 ## load price database
 dbase <- fread("C://Users/leandro/Dropbox/Docs/Investigacion/2016.Distance and quality/Bases/2020.Finales/2020-dbase-original.csv", data.table = F)
+dbase <- fread("~/Dropbox/Docs/Investigacion/2016.Distance and quality/Bases/2020.Finales/2020-dbase-original.csv", data.table = F)
 head(dbase)
 
 # Database information --
@@ -35,23 +36,21 @@ dbase$moda <- log(dbase$PMode) *100 # Add logs of prices and * 100
 #### Add information of Category ---------------------------
 
 # Product information is on a separate file
-products <- read_excel("~/Dropbox/Docs/Investigacion/2016.Distance and quality/Bases/lista_productos_2014_web_MEF_empresas_lz.xls")
+products <- read_excel("~/Dropbox/Docs/Investigacion/2016.Distance and quality/Bases/2020.Finales/products.xls")
 products <- read_excel("c://Users/leandro/Dropbox/Docs/Investigacion/2016.Distance and quality/Bases/2020.Finales/products.xls")
 head(products)
 
-categ <- distinct(products[, c(1,5,9)], id, Producto, Used)
+categ <- distinct(products[, c(1,6,9,13)], id, Market, Used,`IPC-class`)
 
 # Database information (products) --
 # id: product id number identifier
 # Category: the product category of the good
 # Used: 1 if keep, 0 is skiped
+# IPC-class: broad category to construct instrument
 
-colnames(categ) <- c("Product","Category","Used")
+colnames(categ) <- c("Product","Category","Used","IPC.class")
 dbase <- merge(dbase, categ, by= "Product")  
 head(dbase)
-dbase$Category.y <- NULL
-
-
 # Restrict database
 dbase <- dbase[dbase$Used ==1,]
 dbase$Used <- NULL
@@ -64,16 +63,14 @@ dbase$Varieties <- 0
 dbase$Varieties <- with(dbase, ave(Product, Category, Time, Super, FUN= length)) - 1
 
 
-
 #### Add common supermarket information -------------
 
-
 ## Load database
-supers <- read.csv("~/Dropbox/Docs/Investigacion/2016.Distance and quality/Bases/Establecimientos.csv", header = T, sep = ',')
+supers <- read.csv("~/Dropbox/Docs/Investigacion/2016.Distance and quality/Bases/2020.Finales/Establecimientos.csv", header = T, sep = ',')
 supers <- read.csv("C:/Users/leandro/Dropbox/Docs/Investigacion/2016.Distance and quality/Bases/2020.Finales/Establecimientos.csv") #windows
 head(supers)
 
-supers <- supers[, c("Super", "chain.number", "city.number", "depto.number", "X_UTM", "Y_UTM")] ## the information needed to merge
+supers <- supers[, c("Super", "chain.number", "city.number", "depto.number", "X_UTM", "Y_UTM","ccz")] ## the information needed to merge
 
 # Database information (products) --
 # Super: store id number identifier
@@ -82,6 +79,7 @@ supers <- supers[, c("Super", "chain.number", "city.number", "depto.number", "X_
 # depto.number: the id of the state (departamento) where the store is located (not used)
 # X_UTM: the location in the easting UTM
 # Y_UTM: the location in the northing UTM
+# ccz: political location in Montevideo
 
 supers <- supers[order(as.numeric(supers$Super)) ,] # order by supermarket
 
@@ -109,6 +107,13 @@ supers <- supers[supers$Super != 386,]
 dbase <- merge(dbase, supers, by = "Super") # Merge cola and supermarket bases
 rm(supers, not.in.prices, not.in.supers)
 
-
 ## Export databases
 write.csv(dbase, "C:/Users/leandro/Dropbox/Docs/Investigacion/2016.Distance and quality/Bases/2020.Finales/2020-dbase-levels-final.csv", row.names = FALSE)
+write.csv(dbase, "~/Dropbox/Docs/Investigacion/2016.Distance and quality/Bases/2020.Finales/2020-dbase-levels-final.csv", row.names = FALSE)
+
+
+### Start next script ----------------
+
+source("/path/to/file/2-difference-database-2020.R")
+
+#### End of script ---------------------------------------
